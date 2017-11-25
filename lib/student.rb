12804a9b09d0d22155fcb5ec2ff1a84b1d342e7c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-11-25 00:58:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,6 @@
 class Student
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
